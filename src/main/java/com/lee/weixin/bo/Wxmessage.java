42da71b60eaa3c0efe51f755c212c5d3cf43976a
liwenjing <liwenjing@caishuo.com>
2016-02-29 19:03:34 +0800

    111

---
+++ src/main/java/com/lee/weixin/bo/Wxmessage.java
@@ -0,0 +1,117 @@
+package com.lee.weixin.bo;
+
+public class Wxmessage {
+	public String FromUserName;
+	public String ToUserName;
+	public String MsgType;
+	public String EventName;
+	public String Content;
+	public String Recognition;
+	public String MediaId;
+	public String EventKey;
+	public String Location_X;
+	public String Location_Y;
+	public String Scale;
+	public String Label;
+	
+    public String Latitude;
+    public String Longitude;
+    public String Precision;
+    
+    
+    
+    
+	public String getLatitude() {
+		return Latitude;
+	}
+	public void setLatitude(String latitude) {
+		Latitude = latitude;
+	}
+	public String getLongitude() {
+		return Longitude;
+	}
+	public void setLongitude(String longitude) {
+		Longitude = longitude;
+	}
+	public String getPrecision() {
+		return Precision;
+	}
+	public void setPrecision(String precision) {
+		Precision = precision;
+	}
+	public String getFromUserName() {
+		return FromUserName;
+	}
+	public void setFromUserName(String fromUserName) {
+		FromUserName = fromUserName;
+	}
+	public String getToUserName() {
+		return ToUserName;
+	}
+	public void setToUserName(String toUserName) {
+		ToUserName = toUserName;
+	}
+	public String getMsgType() {
+		return MsgType;
+	}
+	public void setMsgType(String msgType) {
+		MsgType = msgType;
+	}
+	public String getEventName() {
+		return EventName;
+	}
+	public void setEventName(String eventName) {
+		EventName = eventName;
+	}
+	public String getContent() {
+		return Content;
+	}
+	public void setContent(String content) {
+		Content = content;
+	}
+	public String getRecognition() {
+		return Recognition;
+	}
+	public void setRecognition(String recognition) {
+		Recognition = recognition;
+	}
+	public String getMediaId() {
+		return MediaId;
+	}
+	public void setMediaId(String mediaId) {
+		MediaId = mediaId;
+	}
+	public String getEventKey() {
+		return EventKey;
+	}
+	public void setEventKey(String eventKey) {
+		EventKey = eventKey;
+	}
+	public String getLocation_X() {
+		return Location_X;
+	}
+	public void setLocation_X(String location_X) {
+		Location_X = location_X;
+	}
+	public String getLocation_Y() {
+		return Location_Y;
+	}
+	public void setLocation_Y(String location_Y) {
+		Location_Y = location_Y;
+	}
+	public String getScale() {
+		return Scale;
+	}
+	public void setScale(String scale) {
+		Scale = scale;
+	}
+	public String getLabel() {
+		return Label;
+	}
+	public void setLabel(String label) {
+		Label = label;
+	}
+	
+	
+
+}
