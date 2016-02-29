7748b3660f32329b2e1617877d1db152d4e5d12a
liwenjing <liwenjing@caishuo.com>
2016-02-29 17:41:41 +0800

    post add

--- src/main/java/com/lee/weixin/action/ApiAction.java
+++ src/main/java/com/lee/weixin/action/ApiAction.java
@@ -1,25 +1,25 @@
 package com.lee.weixin.action;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import io.netty.handler.codec.http.HttpResponse;
 
 @Controller
-@RequestMapping("/api/weixin")
+@RequestMapping("/api/")
 public class ApiAction {
 
-	@RequestMapping(value = "/checkServer.json", method = GET)
-	public void get(String signature, String timestamp, String nonce, String echostr,HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/weixin", method = GET)
+	public void get(String signature, String timestamp, String nonce, String echostr, HttpServletResponse response)
+			throws Exception {
 
 		ArrayList<String> arl = new ArrayList<String>();
 		arl.add("abc123");
@@ -40,11 +40,18 @@ public class ApiAction {
 		if (byte2hex(digesta).equals(signature)) {
 			System.out.println("信息检查正常");
 		} else {
-			System.err.println("收到的消息是：" + signature );
+			System.err.println("收到的消息是：" + signature);
 			System.out.println("摘要不相同");
 		}
 		System.out.println(echostr);
 		response.getWriter().print(echostr);
+	}
+
+	@RequestMapping(value = "/weixin", method = POST)
+	public void post(@RequestBody String requestBody, HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		System.out.println(requestBody);
+		response.getWriter().print("success");
 	}
 
 	public String byte2hex(byte[] b) // 二行制转字符串
