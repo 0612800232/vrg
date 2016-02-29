64b415c05cafdad2c6cfbfccf3ac39cee9c27d1c
liwenjing <liwenjing@caishuo.com>
2016-02-29 19:03:20 +0800

    1

--- src/main/java/com/lee/weixin/action/ApiAction.java
+++ src/main/java/com/lee/weixin/action/ApiAction.java
@@ -2,20 +2,29 @@ package com.lee.weixin.action;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.lee.weixin.bo.Wxmessage;
+
 @Controller
 @RequestMapping("/api/")
 public class ApiAction {
+	Logger log = Logger.getLogger(ApiAction.class);
 
 	@RequestMapping(value = "/weixin", method = GET)
 	public void get(String signature, String timestamp, String nonce, String echostr, HttpServletResponse response)
@@ -51,7 +60,72 @@ public class ApiAction {
 	public void post(@RequestBody String requestBody, HttpServletRequest request, HttpServletResponse response)
 			throws Exception {
 		System.out.println(requestBody);
-		response.getWriter().print("success");
+		sendMessage(getWxMessage(requestBody), response);
+	}
+
+	void sendMessage(Wxmessage wxmessage, HttpServletResponse response) throws Exception {
+
+		Document xml = DocumentHelper.createDocument();
+		Element root = xml.addElement("xml");
+		
+		Element ToUserName = root.addElement("ToUserName");
+		ToUserName.setText(addText(wxmessage.getFromUserName()));
+
+		Element FromUserName = root.addElement("FromUserName");
+		FromUserName.setText(addText(wxmessage.getToUserName()));
+
+		Element CreateTime = root.addElement("CreateTime");
+		CreateTime.setText(addText(String.valueOf(new Date().getTime())));
+
+		Element MsgType = root.addElement("MsgType");
+		MsgType.setText(addText("text"));
+
+		Element Content = root.addElement("Content");
+		String content = "您发送的位置是:" + wxmessage.getLabel() + ";纬度是:" + wxmessage.getLocation_X() + ";经度是:"
+				+ wxmessage.getLocation_Y() + ";缩放比例为:" + wxmessage.getScale();
+		Content.setText(addText(content));
+		response.getWriter().print(xml.asXML());
+
+	}
+
+	String addText(String text) {
+		return "![CDATA[" + text + "]]";
+	}
+
+	Wxmessage getWxMessage(String requestBody) throws Exception {
+		Wxmessage wx = new Wxmessage();
+		Document xml = DocumentHelper.parseText(requestBody);
+		wx.ToUserName = xml.getRootElement().elementTextTrim("ToUserName");
+		wx.FromUserName = xml.getRootElement().elementTextTrim("FromUserName");
+		wx.MsgType = xml.getRootElement().elementTextTrim("MsgType");
+		log.info("MsgType:" + wx.MsgType);
+		if ("event".equals(wx.MsgType.trim())) {
+			wx.EventName = xml.getRootElement().elementTextTrim("Event");
+			log.info(wx.EventName);
+			if ("LOCATION".equals(wx.EventName.toUpperCase())) {
+				wx.Latitude = xml.getRootElement().elementTextTrim("Latitude");
+				wx.Longitude = xml.getRootElement().elementTextTrim("Longitude");
+				wx.Precision = xml.getRootElement().elementTextTrim("Precision");
+			} else {
+				wx.EventKey = xml.getRootElement().elementTextTrim("EventKey");
+			}
+		}
+		if ("text".equals(wx.MsgType.trim())) {
+			wx.setContent(xml.getRootElement().elementTextTrim("Content"));
+		}
+		if ("location".equals(wx.MsgType.trim())) {
+			wx.Location_X = xml.getRootElement().elementTextTrim("Location_X");
+			wx.Location_Y = xml.getRootElement().elementTextTrim("Location_Y");
+			wx.Scale = xml.getRootElement().elementTextTrim("Scale");
+			wx.Label = xml.getRootElement().elementTextTrim("Label");
+
+		}
+
+		if ("voice".equals(wx.MsgType.trim())) {
+			wx.Recognition = xml.getRootElement().elementTextTrim("Recognition");
+		}
+
+		return wx;
 	}
 
 	public String byte2hex(byte[] b) // 二行制转字符串
